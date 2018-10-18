6b6a9afa0d2b46f72953e4c47fc1bf4ba9e2dcd1
MILCUATRO\lcappi <leonardo.cappi@avanzasa.com>
2018-10-18 15:56:11 -0300

    Jobs 10 Rows

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const passport = require("passport");
 const flash = require("connect-flash");
 const cookie = require("cookie-parser");
 const bodyParser = require("body-parser");
-const session = require("express-session");
+const CookieSession = require("cookie-session");
 
 require("./model/sequelize/passport")(passport);
 
@@ -16,10 +16,9 @@ app.set("view engine", "jade");
 //middlewares
 app.use(cookie());
 app.use(bodyParser.urlencoded({extended: false}));
-app.use(session({
-    secret: "elMagoJp69",
-    resave: false,
-    saveUninitialized: false
+app.use(CookieSession({
+    name: "sesion",
+    keys: ["llave-1", "llave-2"]
 }));
 app.use(passport.initialize());
 app.use(passport.session());
